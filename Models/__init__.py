202ecb4634448830c506c0f352709aa5fca6a86a
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-09 20:09:26 +0330

    Update __init__.py

--- Models/__init__.py
+++ Models/__init__.py
@@ -2,16 +2,19 @@
 from .user import User, UserCreate, UserRead, UserUpdate
 from .item import Item, ItemCreate, ItemRead, ItemUpdate
 from .Provider import Provider, ProviderCreate, ProviderRead, ProviderUpdate
-from .order import (
-    Order, OrderCreate, OrderRead, OrderUpdate,
-    OrderItem, OrderItemCreate, OrderItemRead, OrderItemUpdate,
-    OrderStatus
-)
-from .transaction import (
-    Transaction, TransactionCreate, TransactionRead, TransactionUpdate,
-    TransactionType, TransactionStatus
+from .supplier_order import (
+    SupplierOrder, 
+    SupplierOrderCreate, 
+    SupplierOrderRead, 
+    SupplierOrderStatus
 )
 
+from .customer_order import (
+    CustomerOrder,
+    CustomerOrderCreate, 
+    CustomerOrderRead, 
+    CustomerOrderStatus
+)
 # لیست تمام مدل‌ها برای دسترسی عمومی
 __all__ = [
     # User models
@@ -23,14 +26,8 @@ __all__ = [
     # Provider models
     'Provider', 'ProviderCreate', 'ProviderRead', 'ProviderUpdate',
     
-    # Order models
-    'Order', 'OrderCreate', 'OrderRead', 'OrderUpdate',
-    'OrderItem', 'OrderItemCreate', 'OrderItemRead', 'OrderItemUpdate',
-    'OrderStatus',
-    
-    # Transaction models
-    'Transaction', 'TransactionCreate', 'TransactionRead', 'TransactionUpdate',
-    'TransactionType', 'TransactionStatus'
+    'SupplierOrder', 'SupplierOrderCreate', 'SupplierOrderRead', 'SupplierOrderStatus',
+    'CustomerOrder', 'CustomerOrderCreate', 'CustomerOrderRead', 'CustomerOrderStatus'
 ]
 
 # همچنین می‌توانید توابع کمکی رو هم export کنید
@@ -38,25 +35,20 @@ from .user import User
 from .item import Item
 
 # توابع کاربردی
-def get_all_models():
-    """لیست تمام مدل‌های جدول"""
+def get_all_table_models():
+    """لیست تمام مدل‌های جدول (برای ایجاد جداول)"""
+    from .user import User
+    from .item import Item
+    from .Provider import Provider
+    from .supplier_order import SupplierOrder, SupplierOrderItem
+    from .customer_order import CustomerOrder, CustomerOrderItem
+    
     return [
         User,
-        Item,
+        Item, 
         Provider,
-        Order,
-        OrderItem,
-        Transaction
+        SupplierOrder,
+        SupplierOrderItem,
+        CustomerOrder,
+        CustomerOrderItem
     ]
-
-def get_model_by_name(model_name: str):
-    """دریافت مدل بر اساس نام"""
-    models_dict = {
-        'user': User,
-        'item': Item,
-        'provider': Provider,
-        'order': Order,
-        'order_item': OrderItem,
-        'transaction': Transaction
-    }
-    return models_dict.get(model_name.lower())
