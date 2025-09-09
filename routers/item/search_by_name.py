1f372565e206968134fe489f80b767f7747d6b12
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-09 15:17:52 +0330

    M

--- routers/item/search_by_name.py
+++ routers/item/search_by_name.py
@@ -1,5 +1,5 @@
 from sqlmodel import select, Session
-from models.item import Item
+from Models.item import Item
 from database import get_session
 
 def search_items_by_name(name_query: str) -> list[Item]:
