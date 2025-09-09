9d96219d47ffa4cc6cc28fba0b68ef4d4e6460cc
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-09 14:43:17 +0330

    debug

--- routers/item/search_by_sku.py
+++ routers/item/search_by_sku.py
@@ -1,5 +1,5 @@
 from sqlmodel import select, Session
-from models.item import Item
+from Models.item import Item
 from database import get_session
 
 def search_item_by_sku(sku: str) -> Item | None:
