c8dcfd0c2cac43f789a9140c7d91fe454d0523e3
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-09 20:09:52 +0330

    Update tempCodeRunnerFile.py

--- Models/tempCodeRunnerFile.py
+++ Models/tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
+from sqlmodel import SQLModel
 import bcrypt
+
+# در فایل user.py
+import bcrypt  # ✅ اینجا import بشه
+
+class User(SQLModel):
+    def verify_password(self, password: str) -> bool:
+        return bcrypt.checkpw(...)  # ✅ اینجا استفاده بشه
+    
+    #باید حذف بشه
