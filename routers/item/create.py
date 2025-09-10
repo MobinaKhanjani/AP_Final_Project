b0509f82f074205c33469c721a7c49dda048f5ac
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-10 20:47:46 +0330

    Update create.py

--- routers/item/create.py
+++ routers/item/create.py
@@ -5,7 +5,7 @@ from database import get_session
 
 router = APIRouter()
 
-@router.post("/items", response_model=Item)
+@router.post("/create", response_model=Item)
 def create_item(item_data: ItemCreate, session: Session = Depends(get_session)):
     item = Item.from_orm(item_data)
     session.add(item)
