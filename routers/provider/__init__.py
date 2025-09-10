dddef4f5b06dd73c5cd1ba1d228436d2034ea6c0
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-10 18:39:54 +0330

    Update __init__.py

--- routers/provider/__init__.py
+++ routers/provider/__init__.py
@@ -5,5 +5,5 @@ from .delete import router as delete_router
 router = APIRouter(tags=["Providers"])
 
 # شامل کردن روترها
-router.include_router(add_router, prefix="/providers")
-router.include_router(delete_router, prefix="/providers")
+router.include_router(add_router)
+router.include_router(delete_router)
