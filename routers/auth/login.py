3fa93cd37a5b80646cd423392f1f0f447bc923b9
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-10 22:49:10 +0330

    Update login.py

--- routers/auth/login.py
+++ routers/auth/login.py
@@ -9,7 +9,7 @@ router = APIRouter()
 
 @router.post("/login")
 async def login(
-    form_data: OAuth2PasswordRequestForm = Depends(),
+    form_data: OAuth2PasswordRequestForm = Depends(), #+
     session: Session = Depends(get_session)
 ):
     """
