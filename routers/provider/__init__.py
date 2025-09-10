60fb4c895e2988f5360cdd46cef3430c6a368a0e
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-10 17:01:40 +0330

    Update __init__.py

--- routers/provider/__init__.py
+++ routers/provider/__init__.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
-from routers.provider.add import router as add_provider_router
-from routers.provider.delete import router as delete_provider_router
+from .add import router as add_router
+from .delete import router as delete_router
 
-provider_router = APIRouter()
+router = APIRouter(tags=["Providers"])
 
-# ثبت routeهای مربوط به provider
-provider_router.include_router(add_provider_router)
-provider_router.include_router(delete_provider_router)
+# شامل کردن روترها
+router.include_router(add_router, prefix="/providers")
+router.include_router(delete_router, prefix="/providers")
