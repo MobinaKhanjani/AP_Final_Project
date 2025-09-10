346c6aa9d4e07fe269de6dffbb80b18e90556979
MobinaKhanjani <m.khanjany515@gmail.com>
2025-09-10 13:35:43 +0330

    Update delete.py

--- routers/item/delete.py
+++ routers/item/delete.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
-from Models.item import Item
-from database import get_session
+from app.models.item import Item
+from app.database import get_session
 
 router = APIRouter()
 
-@router.delete("/items/delete/{item_id}")
+@router.delete("/items/{item_id}")
 def delete_item(item_id: int, session: Session = Depends(get_session)):
     item = session.get(Item, item_id)
     if not item:
         raise HTTPException(status_code=404, detail="آیتم پیدا نشد")
+
     session.delete(item)
     session.commit()
-    return {"message": "آیتم با موفقیت حذف شد"}
+    return {"detail": "آیتم با موفقیت حذف شد"}
