f4ae7b7249adad2477dc24b7d6f37902f25ff6c8
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-11 20:09:53 +0330

    Update create.py

--- routers/item/create.py
+++ routers/item/create.py
@@ -2,11 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from Models.item import ItemCreate, Item
 from database import get_session
+from security import get_current_user
+from Models.user import User
 
 router = APIRouter()
 
 @router.post("/Create", response_model=Item)
-def create_item(item_data: ItemCreate, session: Session = Depends(get_session)):
+def create_item(item_data: ItemCreate, session: Session = Depends(get_session),current_user: User = Depends(get_current_user)):
     item = Item.from_orm(item_data)
     session.add(item)
     session.commit()
