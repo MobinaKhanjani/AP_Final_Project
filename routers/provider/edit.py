ca39005cd2323819a9d78959a3fa7545b1716b1e
NazaninZahra Sharifi <nzsharifi81@gmail.com>
2025-09-11 20:09:25 +0330

    Update edit.py

--- routers/provider/edit.py
+++ routers/provider/edit.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session
 from Models.Provider import Provider, ProviderUpdate
 from database import get_session
-
+from security import get_current_user
+from Models.user import User
 router = APIRouter()
 
 @router.put("/edit/{provider_id}", response_model=Provider)
-def update_provider(provider_id: int, update_data: ProviderUpdate, session: Session = Depends(get_session)):
+def update_provider(provider_id: int, update_data: ProviderUpdate, session: Session = Depends(get_session),current_user: User = Depends(get_current_user)):
     provider = session.get(Provider, provider_id)
     if not provider:
         raise HTTPException(status_code=404, detail="تأمین‌کننده پیدا نشد")
