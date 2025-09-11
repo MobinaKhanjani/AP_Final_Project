d24a298c93b123c64e2d38f83e6f52ac7020cba9
MobinaKhanjani <m.khanjany515@gmail.com>
2025-09-11 21:13:16 +0330

    Update __init__.py

--- routers/customer_order/__init__.py
+++ routers/customer_order/__init__.py
@@ -1,21 +1,11 @@
 from fastapi import APIRouter
-from .list import router as list_router
 from .create import router as create_router
+from .list import router as list_router
 #from .get import router as get_router
 from .update import router as update_router
 
 router = APIRouter()
-router.include_router(list_router, tags=["Customer Orders"])
 router.include_router(create_router, tags=["Customer Orders"])
+router.include_router(list_router, tags=["Customer Orders"])
 #router.include_router(get_router, tags=["Customer Orders"])
 router.include_router(update_router, tags=["Customer Orders"])
-
-
-#وجود تگز:
-#1. ** grouping در Swagger UI **
-# ** organization در documentation **
-#مرتب‌سازی endpointها در docs
-
-#گروه‌بندی منطقی APIها
-
-#زیباتر شدن documentation
