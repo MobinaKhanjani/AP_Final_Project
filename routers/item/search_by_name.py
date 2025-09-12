028b156d5d318181edca41bc4ae042bc9a9f17de
reyhanrkm <reyhaneh.karbalaei.aa@gmail.com>
2025-09-11 18:28:03 -0800

    debug

--- routers/item/search_by_name.py
+++ routers/item/search_by_name.py
@@ -1,16 +1,3 @@
-#from fastapi import APIRouter, Depends 
-#from sqlmodel import select 
-#from Models.item import Item 
-#from database import get_session 
-#from typing import List 
-#router = APIRouter() 
-#@router.get("/Search_by_name", response_model=List[Item]) 
-#def search_items_by_name(name: str, session=Depends(get_session)): 
-#    query = select(Item).where(Item.name.contains(name))
-#    return session.exec(query).all() 
-#if not item: 
-# #raise HTTPException(status_code=404, detail="کالا با این SKU پیدا نشد") 
-# #return item
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import select
 from Models.item import Item
